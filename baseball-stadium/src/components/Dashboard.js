c04fdc1b4211a14e9035e61649c70878a5bfd07e
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-08 14:36:40 -0700

    everything but the logic set up

--- baseball-stadium/src/components/Dashboard.js
+++ baseball-stadium/src/components/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { sportEvent } from '../sportsball';
 
 export default ({ update }) => {
   return (
     <div>
-      <button onClick={update}>strike</button>
-      <button onClick={update}>ball</button>
-      <button onClick={update}>foul</button>
-      <button onClick={update}>hit</button>
+      <button onClick={() => update(sportEvent.STRIKE)}>strike</button>
+      <button onClick={() => update(sportEvent.BALL)}>ball</button>
+      <button onClick={() => update(sportEvent.FOUL)}>foul</button>
+      <button onClick={() => update(sportEvent.HIT)}>hit</button>
     </div>
   );
 };
